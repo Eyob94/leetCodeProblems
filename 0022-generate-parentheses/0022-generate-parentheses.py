a6a3a75a62b681fa1a1a0b827cb2dd43a6d2e797
Eyob <103565328+Eyob94@users.noreply.github.com>
2023-04-26 20:03:00 +0300

    Time: 48 ms (7.72%), Space: 16.6 MB (8.11%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        dp = [[] for i in range(n + 1)]
+        dp[0].append('')
+        for i in range(n + 1):
+            for j in range(i):
+                dp[i] += ['(' + x + ')' + y for x in dp[j] for y in dp[i - j - 1]]
+        return dp[n]
