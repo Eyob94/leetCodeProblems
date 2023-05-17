01098e60b21dac5169e6ff1a9f2b332b518a96f9
Eyob <103565328+Eyob94@users.noreply.github.com>
2023-05-17 11:47:33 +0300

    Time: 64 ms (84.18%), Space: 20.6 MB (77.51%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,23 +1,35 @@
-# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-
-        if not inorder or not preorder:
+    def buildTree(self, preorder, inorder):
+        if not preorder or not inorder:
             return None
-  
-        head = TreeNode(preorder[0])
-        headIndex = inorder.index(head.val)
-        newPre = preorder[1:]
 
-        head.left  = self.buildTree(preorder[1:headIndex+1], inorder[:headIndex])
-        head.right = self.buildTree(preorder[headIndex+1:], inorder[headIndex+1:])
+        root = TreeNode(preorder[0])
+        stack = [root]
+        inorder_index = 0
 
-        
+        for i in range(1, len(preorder)):
+            preorder_val = preorder[i]
+            node = stack[-1]
 
-        return head
-        
+            if node.val != inorder[inorder_index]:
+                # Create a new node and set it as the left child of the current node
+                node.left = TreeNode(preorder_val)
+                stack.append(node.left)
+            else:
+                # Find the node in the stack whose value does not match the inorder traversal
+                while stack and stack[-1].val == inorder[inorder_index]:
+                    node = stack.pop()
+                    inorder_index += 1
+
+                # Set the next preorder value as the right child of the current node
+                node.right = TreeNode(preorder_val)
+                stack.append(node.right)
+
+        return root
