7ce45089b8925bfb902b464206edc3937d743b89
Eyob <103565328+Eyob94@users.noreply.github.com>
2023-05-17 12:05:09 +0300

    Time: 75 ms (76.37%), Space: 20.8 MB (75.77%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -8,28 +8,24 @@
 class Solution:
     def buildTree(self, preorder, inorder):
         if not preorder or not inorder:
-            return None
-
+            return None;
+        
         root = TreeNode(preorder[0])
         stack = [root]
         inorder_index = 0
 
-        for i in range(1, len(preorder)):
-            preorder_val = preorder[i]
+        for val in preorder[1:]:
             node = stack[-1]
 
             if node.val != inorder[inorder_index]:
-                # Create a new node and set it as the left child of the current node
-                node.left = TreeNode(preorder_val)
+                node.left = TreeNode(val)
                 stack.append(node.left)
             else:
-                # Find the node in the stack whose value does not match the inorder traversal
                 while stack and stack[-1].val == inorder[inorder_index]:
                     node = stack.pop()
                     inorder_index += 1
-
-                # Set the next preorder value as the right child of the current node
-                node.right = TreeNode(preorder_val)
+                
+                node.right = TreeNode(val)
                 stack.append(node.right)
 
         return root
