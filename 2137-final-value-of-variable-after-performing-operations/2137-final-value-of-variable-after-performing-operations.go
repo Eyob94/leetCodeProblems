833589e638b6b3cde805aab91f56162253e21b3d
Eyob <103565328+Eyob94@users.noreply.github.com>
2023-10-19 08:52:16 +0300

    Time: 2 ms (70.41%), Space: 3.5 MB (100%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.go
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.go
@@ -1,14 +1,12 @@
-
 func finalValueAfterOperations(operations []string) int {
-    x:=0
-    for _, v := range operations{
-        r := []rune(v)
-        if r[0]=='-' || r[len(r)-1] == '-'{
-            x--
-            continue
-        }
-            x++
-    }
-
-    return x
+    	x := 0
+	for _, val := range operations {
+		lenStr := utf8.RuneCountInString(val)
+		if string(val[0]) == "-" || string(val[lenStr-1]) == "-" {
+			x--
+			continue
+		}
+		x++
+	}
+	return x
 }
